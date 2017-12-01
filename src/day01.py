5c4f19a917232131a1cd2280193391a914905156
Adrian Bjugård <adrian@qng.se>
2017-12-01 21:24:24 +0100

    Add day 1

--- src/day01.py
+++ src/day01.py
@@ -1,4 +1,13 @@
 from santas_little_helpers import *
 
+def solve_puzzle(arr, offset) -> int:
+  for i in range(0, len(arr)):
+    if arr[i] == arr[(i+offset) % len(arr)]:
+      yield int(arr[i])
+
 if __name__ == '__main__':
-  data = get_data(day(2017, 1), base_ops + [])
+  data = get_data(day(2017, 1), base_ops + [])[0]
+
+  print(f'The value for star 1 = {sum(solve_puzzle(data, 1))}')
+  offset = int(len(data)/2)
+  print(f'The value for star 2 = {sum(solve_puzzle(data, offset))}')
