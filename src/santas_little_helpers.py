20f4c0341a293f0fc6add2ad49030d39fd4d8de0
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2017-12-02 17:14:54 +0100

    Use https

--- src/santas_little_helpers.py
+++ src/santas_little_helpers.py
@@ -50,12 +50,12 @@ def get_data(today: date = date.today(), ops: list = base_ops) -> Iterator:
       yield format_line(line, ops)
 
 def submit_answer(today: date, answer: str, level: int = 1) -> None:
-  url = f'http://adventofcode.com/{today.year}/day/{today.day}/answer'
+  url = f'https://adventofcode.com/{today.year}/day/{today.day}/answer'
   payload = {
     'level': level,
     'answer': answer
   }
-  res = request('POST', url, data=payload, cookies=config)
+  res = request('POST', url, cookies=config, data=payload)
   soup = BeautifulSoup(res.content, 'html.parser')
   for content in soup.find_all('article'):
     print(content.text)
