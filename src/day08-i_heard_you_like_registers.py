5ab0ad9ac5988ac4cf3670104709b86211613286
Adrian Bjugård <adrian@qng.se>
2017-12-08 09:52:09 +0100

    Cleanup day 8

--- src/day08-i_heard_you_like_registers.py
+++ src/day08-i_heard_you_like_registers.py
@@ -1,47 +1,43 @@
 from santas_little_helpers import *
 from collections import defaultdict
+import operator
 
 today = day(2017, 8)
 
 regs = defaultdict(int)
 
+ops = {
+  '==': operator.eq,
+  '!=': operator.ne,
+  '<=': operator.le,
+  '>=': operator.ge,
+  '<':  operator.lt,
+  '>':  operator.gt
+}
+
 class Instruction():
   def __init__(self, arr):
     self.reg = arr[0]
-    self.op = arr[1]
-    self.amt = int(arr[2])
-    self.cond_reg = arr[4]
-    self.cond_op = arr[5]
-    self.cond_amt = int(arr[6])
+    self.run = self.__inc if arr[1] == 'inc' else self.__dec
+    self.__amt = int(arr[2])
+    self.__cond_reg = arr[4]
+    self.__cond_op = ops[arr[5]]
+    self.__cond_amt = int(arr[6])
 
-def condition(inst):
-  reg = inst.cond_reg
-  op = inst.cond_op
-  amt = inst.cond_amt
-  if op == '==':
-    return regs[reg] == amt
-  if op == '!=':
-    return regs[reg] != amt
-  if op == '<=':
-    return regs[reg] <= amt
-  if op == '>=':
-    return regs[reg] >= amt
-  if op == '<':
-    return regs[reg] < amt
-  if op == '>':
-    return regs[reg] > amt
-  else:
-    print(f'missing operator {op}')
-    exit(0)
+  def valid(self):
+    return self.__cond_op(regs[self.__cond_reg], self.__cond_amt)
+
+  def __inc(self):
+    regs[self.reg] += self.__amt
+
+  def __dec(self):
+    regs[self.reg] -= self.__amt
 
 def solve(data):
   highest = 0
   for inst in data:
-    if condition(inst):
-      if inst.op == 'inc':
-        regs[inst.reg] += inst.amt
-      else:
-        regs[inst.reg] -= inst.amt
+    if inst.valid():
+      inst.run()
       if regs[inst.reg] > highest:
         highest = regs[inst.reg]
   return highest
