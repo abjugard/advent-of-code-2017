6daf147f46a38e0d888ef720a7c3d2e57f432a86
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2017-12-18 23:11:19 +0100

    Typing

--- src/day15-dueling_generators.py
+++ src/day15-dueling_generators.py
@@ -1,15 +1,16 @@
 from santas_little_helpers import day, get_data, timed
+from typing import Iterator
 
 today = day(2017, 15)
 
-def generator(val, mult, mod=1):
+def generator(val: int, mult: int, mod: int = 1) -> Iterator:
   while True:
     val *= mult
     val %= 2147483647
     if val % mod == 0:
       yield val & 0xffff
 
-def duel(a_val, b_val):
+def duel(a_val: int, b_val: int) -> (int, int):
   a_mult, b_mult = 16807, 48271
   gen_a, gen_b = generator(a_val, a_mult), generator(b_val, b_mult)
   star1 = sum(next(gen_a) == next(gen_b) for _ in range(40000000))
