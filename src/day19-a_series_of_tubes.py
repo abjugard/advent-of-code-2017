22dabd261a1c86689738c4fea98b6272843ce0b8
Adrian Bjugård <adrian@qng.se>
2017-12-25 14:58:31 +0100

    Split direction class into library

--- src/day19-a_series_of_tubes.py
+++ src/day19-a_series_of_tubes.py
@@ -1,31 +1,11 @@
 from santas_little_helpers import day, get_data, timed
-from enum import Enum
+from direction import Direction, ds
 
 today = day(2017, 19)
 
-class Direction(Enum):
-  NORTH = (0, -1, 'w', 'e')
-  WEST = (-1, 0, 's', 'n')
-  SOUTH = (0, 1, 'e', 'w')
-  EAST = (1, 0, 'n', 's')
+the_map = {}
 
-  def __init__(self, x: int, y: int, left: str, right: str):
-    self.x = x
-    self.y = y
-    self.left = left
-    self.right = right
-
-  def __add__(self, t: tuple):
-    return self.x+t[0], self.y+t[1]
-
-ds = {
-  'n': Direction.NORTH,
-  'e': Direction.EAST,
-  's': Direction.SOUTH,
-  'w': Direction.WEST
-}
-
-def follow(the_map: [[str]]) -> (int, int):
+def follow(the_map: [str]) -> (int, int):
   p = the_map[0].index('|'), 0
   d = Direction.SOUTH
   letters = ''
